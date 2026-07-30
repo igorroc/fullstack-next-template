f6bebf917ebe44cf1e76274e7530007870a5b2e1
Igor MSI <igor_roc@hotmail.com.br>
2026-07-30 00:41:20 -0300

    Update TailwindCSS config to use `@heroui` styles and remove deprecated dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss"
-import { nextui } from "@nextui-org/react"
 
 const config: Config = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@heroui/react/dist/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@heroui/styles/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
@@ -17,6 +17,6 @@ const config: Config = {
 		},
 	},
 	darkMode: "class",
-	plugins: [nextui()],
+	plugins: [],
 }
 export default config
